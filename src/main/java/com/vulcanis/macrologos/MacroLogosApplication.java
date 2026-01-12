8291d1d87945e9444f8bef4fcde56351912f998a
Victor De Vulcanis <victordevulcanis@gmail.com>
2026-01-12 14:16:46 +0100

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/vulcanis/macrologos/MacroLogosApplication.java
@@ -0,0 +1,13 @@
+package com.vulcanis.macrologos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroLogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroLogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vulcanis/macrologos/MacroLogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulcanis.macrologos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroLogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
